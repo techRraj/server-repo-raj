aab941ea1b0035f23575d131d61bcb057b1d64bf
rajkumar chourasiya <rajchourasiya2862002@gmail.com>
2025-07-18 00:51:17 +0530

    Initial commit of server folder

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,5 @@
-import userModel from '../models/userModel.js';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import validator from 'validator';
 import razorpay from 'razorpay';
-import transactionModel from '../models/transactionModel.js';
 
-// Initialize Razorpay
 const razorpayInstance = new razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
